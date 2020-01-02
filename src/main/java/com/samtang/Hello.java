a464ba335a981eb80ed60e321978fe86e59eec00
SamTang <tangman814@gmail.com>
2020-01-02 21:05:48 +0800

    Java and Kotlin variable , Date type

--- src/main/java/com/samtang/Hello.java
+++ src/main/java/com/samtang/Hello.java
@@ -6,5 +6,15 @@ public class Hello {
 //        new Person().Hello();
         Person p = new Person();
         p.Hello();
+
+        int age = 19;
+        Integer age2 = 20;
+        char c = '我';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean aduit = true;
+        boolean enroll = false;
+        String name = "Samtang";
     }
 }
--- src/main/kotlin/com/samtang/Hello.kt
+++ src/main/kotlin/com/samtang/Hello.kt
@@ -5,6 +5,19 @@ fun main() {
 //    Human().Hello()
     val h = Human()
     h.Hello()
+
+    val age = 19 //val 宣告變數後無法更換變數值
+    //age = 20  //指定為 val 重新塞值會錯誤
+
+    var age2 : Int = 19 //val 宣告變數 : 後方可以指定型態
+    age2 = 20   //可以重新塞值
+
+    var weight = 22.6   //無需宣告型態 預設會 double 型態
+    var height = 2.4f   //指定變數型能為 float
+
+    var name : String   //若非一開始塞值給變數, 則必須需告變數型態
+    name = "Samtang"
+
 }
 
 class Human() {
